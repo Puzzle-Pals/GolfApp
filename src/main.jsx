664e0b25e855d09aa250acf91b69d1896848a003
Puzzle-Pals <puzzlepals@hotmail.com>
2025-05-05 01:09:14 -0500

    Add vercel.json and verify Tailwind CSS setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import 'https://cdn.tailwindcss.com';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
